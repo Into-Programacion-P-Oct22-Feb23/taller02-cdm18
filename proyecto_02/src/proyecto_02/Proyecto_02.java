048c29e3ee3b93fde82f449a9445b483e2f8ebd8
Carlos Mejia <cdmejia4@utpl.edu.ec>
2022-10-21 12:12:00 -0500

    mi tercer commit

---
+++ proyecto_02/src/proyecto_02/Proyecto_02.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto_02;
+
+/**
+ *
+ * @author UTPL
+ */
+public class Proyecto_02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      System.out.println("Ejemplo dos básico de Java\n" +
+"Versionado a través de Git\n" +
+"Manejado desde GitHub\n" +
+"Además se usar GitHubClassroom");  
+    }
+    
+}
